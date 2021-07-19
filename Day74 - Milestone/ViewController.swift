ed879c9a010f9309e25b96cbc96b2e9a939b28a4
travisbrigman <travis1000@gmail.com>
2021-07-19 11:18:08 -0500

    Initial Commit

---
+++ Day74 - Milestone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day74 - Milestone
+//
+//  Created by Travis Brigman on 7/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

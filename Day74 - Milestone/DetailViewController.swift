cbc3bd53d5613ba7cabd6b61fefc63e43991ceec
travisbrigman <travis1000@gmail.com>
2021-07-19 14:26:16 -0500

    working detail view

---
+++ Day74 - Milestone/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  Day74 - Milestone
+//
+//  Created by Travis Brigman on 7/19/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var textView: UITextView!
+    
+    var selectedNote: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        textView.text  = selectedNote
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Day74 - Milestone/ViewController.swift
+++ Day74 - Milestone/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var notes = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        notes.append("Test")
+        notes.append("note")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let singleNote = notes[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        cell.textLabel?.text = singleNote
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            
+            vc.selectedNote = notes[indexPath.row]
+
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
